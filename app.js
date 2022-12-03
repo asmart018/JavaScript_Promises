6dc39fede3cfa64a19c88ea432f421244e14511f
Austin Smart <asmart018@gmail.com>
2022-12-03 15:42:55 -0500

    JS Promises Complete

--- app.js
+++ app.js
@@ -15,10 +15,17 @@ function getList() {
   });
 }
 
-// TODO: Handle the resolved or rejected states of the promise
+let errorId = document.getElementById("error");
+let listId = document.getElementById("list");
 
-// TODO: If the promise resolves with the list of hobbits
-// Render the list of hobbits as list items within the unordered list with id="list" (check the index.html file)
-
-// TODO: If the promise rejects with the failure object
-// Display the failure message in the paragraph element with id="error" (check index.html file)
+getList()
+  .then((result) => {
+    result.forEach((element) => {
+      let li = document.createElement("li");
+      li.textContent = element;
+      listId.appendChild(li);
+    });
+  })
+  .catch((result) => {
+    errorId.textContent = result.message;
+  });
